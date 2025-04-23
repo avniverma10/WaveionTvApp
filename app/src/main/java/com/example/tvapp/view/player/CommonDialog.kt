931df38e37c8d7cf4956d1df16c9c0c1416b94f2
avniverma <avni.v@waveiontechnologies.com>
2025-04-23 15:31:17 +0530

    Made a common dialog for errors,logout and exit app

--- app/src/main/java/com/example/tvapp/view/player/CommonDialog.kt
+++ app/src/main/java/com/example/tvapp/view/player/CommonDialog.kt
@@ -3,6 +3,7 @@ package com.example.tvapp.view.player
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.*
@@ -11,19 +12,17 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import com.example.tvapp.R
 
-/**
- * A reusable common dialog with optional title, message, error details, dynamic border color,
- * and optional confirm/dismiss buttons. Any null parameter will hide its UI element.
- * The icon is always shown next to the title.
- */
 @Composable
 fun CommonDialog(
     showDialog: Boolean,
@@ -51,22 +50,17 @@ fun CommonDialog(
             color = containerColor,
             modifier = Modifier
                 .padding(24.dp)
-                .widthIn(max = 300.dp)
-                .wrapContentHeight()
-//                .wrapContentSize()
-                .border(BorderStroke(2.dp, borderColor), RoundedCornerShape(16.dp))
+                .wrapContentWidth()
+                .border(BorderStroke(1.dp, borderColor), RoundedCornerShape(16.dp))
         ) {
             Column(
                 modifier = Modifier.padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
-//                horizontalAlignment = if (noButtons) Alignment.CenterHorizontally else Alignment.Start
             ) {
-                // Title with icon always visible
                 title?.let {
                     Row(
                         modifier = if (noButtons) Modifier.fillMaxWidth() else Modifier,
                         horizontalArrangement = Arrangement.Center,
-//                        horizontalArrangement = if (noButtons) Arrangement.Center else Arrangement.Start,
                         verticalAlignment = Alignment.CenterVertically
                     ) {
                         Image(
@@ -75,25 +69,25 @@ fun CommonDialog(
                             modifier = Modifier.size(24.dp),
                             colorFilter = ColorFilter.tint(Color.White)
                         )
-                        Spacer(modifier = Modifier.width(8.dp))
+                        Spacer(modifier = Modifier.width(10.dp))
                         Text(
                             text = it,
-                            style = MaterialTheme.typography.titleMedium,
+                            fontSize = 19.sp,
                             color = titleColor,
                             textAlign = TextAlign.Center
                         )
                     }
-                    Spacer(modifier = Modifier.height(8.dp))
+                    Spacer(modifier = Modifier.height(17.dp))
                 }
 
                 // Main message
                 message?.let {
+                    val msgFontSize = if (errorCode == null && errorMessage == null) 19.sp else 14.sp
                     Text(
                         text = it,
-                        style = MaterialTheme.typography.bodyMedium,
+                        fontSize = msgFontSize,
                         color = contentColor,
                         textAlign =  TextAlign.Center ,
-//                        textAlign = if (noButtons) TextAlign.Center else TextAlign.Start,
                         modifier = if (noButtons) Modifier.fillMaxWidth() else Modifier
                     )
                     Spacer(modifier = Modifier.height(8.dp))
@@ -105,43 +99,74 @@ fun CommonDialog(
                     val msgText = errorMessage.orEmpty()
                     Text(
                         text = "Error $codeText: $msgText",
-                        style = MaterialTheme.typography.bodySmall,
+                        fontSize = 13.sp,
                         color = contentColor,
                         textAlign =  TextAlign.Center,
-//                        textAlign = if (noButtons) TextAlign.Center else TextAlign.Start,
                         modifier = if (noButtons) Modifier.fillMaxWidth() else Modifier
                     )
-                    Spacer(modifier = Modifier.height(10.dp))
+                    Spacer(modifier = Modifier.height(38.dp))
+                }
+                if ((errorCode == null && errorMessage == null) && !noButtons) {
+                    Spacer(modifier = Modifier.height(30.dp))
                 }
 
                 // Buttons row
                 if (!noButtons) {
-                    val dismissInteraction = remember { MutableInteractionSource() }
-                    val isDismissFocused by dismissInteraction.collectIsFocusedAsState()
-                    val confirmInteraction = remember { MutableInteractionSource() }
-                    val isConfirmFocused by confirmInteraction.collectIsFocusedAsState()
+                    val dismissRequester    = remember { FocusRequester() }
+                    val dismissInteraction  = remember { MutableInteractionSource() }
+                    val isDismissFocused    by dismissInteraction.collectIsFocusedAsState()
+                    val confirmInteraction  = remember { MutableInteractionSource() }
+                    val isConfirmFocused    by confirmInteraction.collectIsFocusedAsState()
+
+                    LaunchedEffect(showDialog) {
+                        if (showDialog) dismissRequester.requestFocus()
+                    }
+
                     Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.End,
-                        verticalAlignment = Alignment.CenterVertically
+                        Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment   = Alignment.CenterVertically
                     ) {
                         dismissButtonText?.let { text ->
                             onDismiss?.let { action ->
-                                TextButton(onClick = action) {
-                                    Text(text, color = Color.White)
+                                TextButton(
+                                    onClick            = action,
+                                    interactionSource  = dismissInteraction,
+                                    modifier           = Modifier
+                                        .focusRequester(dismissRequester)
+                                        .focusable(interactionSource = dismissInteraction),
+                                    shape = RoundedCornerShape(8.dp),
+                                    colors             = ButtonDefaults.textButtonColors(
+                                        containerColor = if (isDismissFocused) Color(0xFF49FEDD) else Color(0xFF414857),
+                                        contentColor   = Color.Black
+                                    )
+                                ) {
+                                    Text(text)
                                 }
-                                Spacer(modifier = Modifier.width(10.dp))
+                                Spacer(Modifier.width(28.dp))
                             }
                         }
+
                         confirmButtonText?.let { text ->
                             onConfirm?.let { action ->
-                                TextButton(onClick = action) {
-                                    Text(text, color = Color.White)
+                                TextButton(
+                                    onClick            = action,
+                                    interactionSource  = confirmInteraction,
+                                    modifier           = Modifier
+                                        .focusable(interactionSource = confirmInteraction),
+                                    shape = RoundedCornerShape(8.dp),
+                                    colors             = ButtonDefaults.textButtonColors(
+                                        containerColor = if (isConfirmFocused) Color(0xFF49FEDD) else Color(0xFF414857),
+                                        contentColor   = Color.Black
+                                    )
+                                ) {
+                                    Text(text)
                                 }
                             }
                         }
                     }
                 }
+
             }
         }
     }
