fdd8caec2a99ece973a4b644587585db37517d77
avniverma <avni.v@waveiontechnologies.com>
2025-08-04 12:40:47 +0530

    Icon color not reflecting issue resolved

--- app/src/main/java/com/tccl/tvapp/utils/Constants.kt
+++ app/src/main/java/com/tccl/tvapp/utils/Constants.kt
@@ -5,6 +5,6 @@ import com.tccl.tvapp.model.data.genre.WTVGenre
 object Constants {
     var genre:List<WTVGenre>?= null
     const val HEADER_TOKEN = "BUAA8JJkzfMI56y4BhEhU"
-    const val BASE_ICON_URL = "https://api-demo.caastv.com/"
+    const val BASE_ICON_URL = "https://api-tccl.caastv.com/"
 
 }
